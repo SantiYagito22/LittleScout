fd438c1bef049e19ea4b50178874db1467ae581c
Santiago Muñoz Castro <santiyagito22@gmail.com>
2024-10-08 13:46:54 +0200

    feat: Token implementation

--- src/app/api/oauth/callback/route.ts
+++ src/app/api/oauth/callback/route.ts
@@ -13,6 +13,27 @@ export async function GET(request: Request) {
     if (!code) {
       throw new Error("Authorization code is missing");
     }
+
+    const response = await fetch('https://id.twitch.tv/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        'client_id': process.env.NEXT_PUBLIC_CLIENT_ID!,
+        'client_secret': process.env.CLIENT_SECRET!,
+        'code': code,
+        'grant_type': 'client_credentials',
+        'redirect_uri': process.env.NEXT_PUBLIC_REDIRECT_URL!
+
+      })
+    })
+
+    const data= await response.json()
+
+    const redirectUrl = new URL('/dashboard', request.url).toString();
+    return NextResponse.redirect(redirectUrl)
+
   } catch(err: any) {
     console.error('OAuth Error:', err.message)
 
