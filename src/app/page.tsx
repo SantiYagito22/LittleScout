2cbaa3ab15f1c9f929dbc059b67b647948ef52ec
Santiago Muñoz Castro <santiyagito22@gmail.com>
2024-10-15 15:00:59 +0200

    feat: New cookies implementation(middleware) with app_access_token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,30 @@
+'use client'
 import Link from 'next/link'
-import { cookies } from 'next/headers'
 
 export default function Home() {
-  const cookieStore = cookies()
-  const redirectRoute = cookieStore.has('access_token')
-    ? '/dashboard'
-    : '/login'
+  function handleTwitchAuth() {
+    const clientId = process.env.NEXT_PUBLIC_CLIENT_ID
+    const redirectUrl = process.env.NEXT_PUBLIC_REDIRECT_URL
+
+    const twitchUrlAuthentication = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&response_type=code&scope=user:read:email`
+
+    window.location.href = twitchUrlAuthentication
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-7xl font-bold">Little Scout</h1>
-      <Link
-        className="text-xl font-semibold rounded-lg bg-button text-button_text p-3 hover:opacity-90 hover:bg-secondary_color"
-        href={redirectRoute}
+      <h1 className="text-headline text-7xl font-bold">Little Scout</h1>
+      {/* <button
+        className="text-xl font-bold rounded-lg bg-button text-button_text p-3 hover:opacity-90 hover:bg-secondary_color"
+        onClick={handleTwitchAuth}
       >
-        Start exploring
+        Authenticate with Twitch
+      </button> */}
+      <Link
+        href={'dashboard'}
+        className="text-xl font-bold rounded-lg bg-button text-button_text p-3 hover:opacity-90 hover:bg-secondary_color"
+      >
+        Start without authentication
       </Link>
     </div>
   )
---
+++ src/middleware.ts
@@ -0,0 +1,63 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function middleware(req: NextRequest) {
+  const tokenExpiry: number = parseInt(req.cookies.get('app_token_expiry')?.value ?? '0', 10)
+  if (!req.cookies.has('app_access_token') || Date.now() >= tokenExpiry) {
+    console.log('We need new token')
+    const response = await fetch('https://id.twitch.tv/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID!,
+        client_secret: process.env.CLIENT_SECRET!,
+        grant_type: 'client_credentials',
+      }),
+    })
+    const data = await response.json()
+    const expiryDate = new Date(Date.now() + data.expires_in)
+    const newResponse = NextResponse.next()
+    newResponse.cookies.set('app_access_token', data.access_token, {expires: expiryDate })
+    newResponse.cookies.set('app_token_expiry', expiryDate.toString() , {expires: expiryDate })
+    
+    return newResponse
+  }
+
+  console.log('We already have token')
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+ 
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      has: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+ 
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      has: [{ type: 'header', key: 'x-present' }],
+      missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
+    },
+  ],
+}
